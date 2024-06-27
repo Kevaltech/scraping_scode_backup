ed829aac11de428f9daa3bca563e4185ffbed978
Kevaltech <158024906+Kevaltech@users.noreply.github.com>
2024-06-27 12:37:51 +0530

    some changes in thefdrate_of utkarsh bank

--- fdrate_date_scraping/fd_date_scraping_utkarsh_bank.py
+++ fdrate_date_scraping/fd_date_scraping_utkarsh_bank.py
@@ -1,32 +1,35 @@
 import requests 
 from bs4 import BeautifulSoup 
-from pypdf import PdfReader 
-from pdfquery import PDFQuery
-from PyPDF2 import PdfFileReader
 import io 
 import datefinder 
 from urllib.request import urlopen 
-from datetime import datetime
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
 
-url = "https://www.utkarsh.bank/xsite/assests/pdf/Fixed_Deposit_Rates_wef_August_21_2023_Website.pdf"
+url = "https://www.utkarsh.bank/personal/term-deposits/fixed-deposits"
 bcode = 311
 def get_date():
     
     try:
-        res = requests.get(url)
-        if res.status_code==200:
-            pdf = PdfFileReader(io.BytesIO(res.content))
-            info = pdf.getDocumentInfo()
-            dates = datefinder.find_dates(info['/Title'])
-            redate = ""
-            for date in dates:
-                redate += date.strftime("%d-%b-%y")
-            return redate , bcode
-
-        else:
-            return "", bcode
+        driver.get(url)
+        driver.implicitly_wait(10)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        content = soup.find("div", id="interest-rate-tab")
+        cn = content.find("p", class_="h5 TTNorms-400 p16")
+        val = cn.find_all('a')
+        # print(cn, val)
+        link = val[0]['href']
+        # print(link[67:71]+'-'+link[72:74]+'-'+link[75:79])
+        dates = datefinder.find_dates(link[67:71]+'-'+link[72:74]+'-'+link[75:79])
+        redate = ""
+        for date in dates:
+            redate += date.strftime("%d-%b-%y")
+        driver.quit()
+        return redate, bcode
     except:
         return "", bcode
 
 
 # ans = get_date()
+# print(ans)
